2d2ac467fe61c5d3f1a0b815201c7d4e8880d15b
DESKTOP-2US7FF4\Michael <arvi_1@gmx.de>
2019-04-13 19:47:12 +0200

    Works only in Unity

---
+++ MovieRandomizer/Assets/Scripts/Mechanic.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+namespace MovieRandomizer
+{
+    public class Mechanic : MonoBehaviour
+    {
+        int randomNumber;
+        public GameObject textFeld;
+        XMLReader myXMLReader;
+
+        // Use this for initialization
+        void Start()
+        {
+            //Debug.Log("Start");
+            myXMLReader = new XMLReader();
+        }
+        // Update is called once per frame
+        //void Update () {
+        //       Debug.Log("Update");
+        //   }
+
+        public void ClickCode()
+        {
+            randomNumber = Random.Range(1,1000);
+            //Debug.Log(randomNumber);
+            textFeld.GetComponent<Text>().text = myXMLReader.GetMovies(randomNumber);
+        }
+
+    }
+}
---
+++ MovieRandomizer/Assets/Scripts/XMLReader.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+using System.Text;
+using System.Xml;
+namespace MovieRandomizer
+{
+    public class XMLReader : MonoBehaviour
+    {
+        XmlReader xmlReader;
+        // Use this for initialization
+        //void Start()
+        //{
+        //    //XmlReader xmlReader = XmlReader.Create("F:/Repository/Unity/MovieRandomizer/MovieRandomizer/Assets/Database/MovieDatabase1000.xml");
+        //    //XmlReader xmlReader = XmlReader.Create(Application.dataPath + "/MovieDatabase1000.xml");
+        //    //xmlReader = XmlReader.Create("http://meinerseitz.de/RandoMovie/MovieDatabase1000.xml");
+        //}
+        // Update is called once per frame
+        //void Update()
+        //{
+        //}
+
+        public String GetMovies(int randomNumber)
+        {
+            xmlReader = XmlReader.Create("http://meinerseitz.de/RandoMovie/MovieDatabase1000.xml");
+            while (xmlReader.Read())
+            {
+                if ((xmlReader.NodeType == XmlNodeType.Element) && (xmlReader.Name == "Movie"))
+                {
+                    if (xmlReader.HasAttributes)
+                        if (xmlReader.GetAttribute("ID")==randomNumber.ToString())
+                            return ("Title: " + xmlReader.GetAttribute("title") + "\n" + "Director: " + xmlReader.GetAttribute("director") + "\n" + "Year: " + xmlReader.GetAttribute("year") + "\n" + "Country: " + xmlReader.GetAttribute("country"));
+                }
+            }
+            return ("");
+        }
+        //{
+        //    XmlReader xmlReader = XmlReader.Create("F:/Repository/Unity/MovieRandomizer/MovieRandomizer/Assets/Database/MovieDatabase.xml");
+        //    Debug.Log(xmlReader.ReadString());
+        //}
+    }
+}
