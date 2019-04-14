40354ecfe82c50d148db16e0b5a27b09e0c61a1e
DESKTOP-2US7FF4\Michael <arvi_1@gmx.de>
2019-04-14 19:02:52 +0200

    * added animation * added sound, doesnt work yet

--- MovieRandomizer/Assets/Scripts/Mechanic.cs
+++ MovieRandomizer/Assets/Scripts/Mechanic.cs
@@ -10,14 +10,19 @@ namespace MovieRandomizer
     {
         int randomNumber;                       //hält die zufällig generierte Nummer zur Auswahl einer FilmID
         public GameObject textFeld;             //hält das GameObject des Textfeldes das den ausgesuchten Film anzeigen soll
+        public GameObject thisButton;
         XMLReader myXMLReaderClass;             //unsere eigene XMLReader Klasse wird deklariert
         string xmlFile;                         //Deklariert den String der dann die komplette MovieDatabase-XML-File als String bereit hält
+        
 
         // Use this for initialization
         void Start()
         {
             myXMLReaderClass = new XMLReader();
             xmlFile = Resources.Load<TextAsset>("MovieDatabase1000").ToString();       //läd die XML File direkt aus dem serialisierten Resourcen Ordner in eine zwischen var
+            var audioClip = Resources.Load<AudioClip>("CranialMenu");
+            this.GetComponent<AudioSource>().clip = audioClip;
+            this.GetComponent<AudioSource>().Play();
         }
         // Update is called once per frame
         //void Update () {
@@ -26,6 +31,7 @@ namespace MovieRandomizer
 
         public void ClickCode()
         {
+            thisButton.GetComponent<Animation>().Play();
             randomNumber = Random.Range(1,1000);
             textFeld.GetComponent<Text>().text = myXMLReaderClass.GetMovies(randomNumber, xmlFile);
         }
