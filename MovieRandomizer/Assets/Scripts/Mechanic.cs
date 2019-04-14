0bbb6df5cd5f01d3950046bbb842bd1083f1880b
DESKTOP-2US7FF4\Michael <arvi_1@gmx.de>
2019-04-14 17:38:50 +0200

    Its Alive!!! * füttern jetzt die XMLl Data aus dem ResourceFolder als String

--- MovieRandomizer/Assets/Scripts/Mechanic.cs
+++ MovieRandomizer/Assets/Scripts/Mechanic.cs
@@ -1,20 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Xml;
 using UnityEngine;
 using UnityEngine.UI;
 namespace MovieRandomizer
 {
     public class Mechanic : MonoBehaviour
     {
-        int randomNumber;
-        public GameObject textFeld;
-        XMLReader myXMLReader;
+        int randomNumber;                       //hält die zufällig generierte Nummer zur Auswahl einer FilmID
+        public GameObject textFeld;             //hält das GameObject des Textfeldes das den ausgesuchten Film anzeigen soll
+        XMLReader myXMLReaderClass;             //unsere eigene XMLReader Klasse wird deklariert
+        string xmlFile;                         //Deklariert den String der dann die komplette MovieDatabase-XML-File als String bereit hält
 
         // Use this for initialization
         void Start()
         {
-            //Debug.Log("Start");
-            myXMLReader = new XMLReader();
+            myXMLReaderClass = new XMLReader();
+            xmlFile = Resources.Load<TextAsset>("MovieDatabase1000").ToString();       //läd die XML File direkt aus dem serialisierten Resourcen Ordner in eine zwischen var
         }
         // Update is called once per frame
         //void Update () {
@@ -24,8 +27,7 @@ namespace MovieRandomizer
         public void ClickCode()
         {
             randomNumber = Random.Range(1,1000);
-            //Debug.Log(randomNumber);
-            textFeld.GetComponent<Text>().text = myXMLReader.GetMovies(randomNumber);
+            textFeld.GetComponent<Text>().text = myXMLReaderClass.GetMovies(randomNumber, xmlFile);
         }
 
     }
--- MovieRandomizer/Assets/Scripts/XMLReader.cs
+++ MovieRandomizer/Assets/Scripts/XMLReader.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 using System;
 using System.Text;
 using System.Xml;
+using System.IO;
+
 namespace MovieRandomizer
 {
     public class XMLReader : MonoBehaviour
@@ -16,29 +18,26 @@ namespace MovieRandomizer
         //    //XmlReader xmlReader = XmlReader.Create("F:/Repository/Unity/MovieRandomizer/MovieRandomizer/Assets/Database/MovieDatabase1000.xml");
         //    //XmlReader xmlReader = XmlReader.Create(Application.dataPath + "/MovieDatabase1000.xml");
         //    //xmlReader = XmlReader.Create("http://meinerseitz.de/RandoMovie/MovieDatabase1000.xml");
+        //  https://drive.google.com/file/d/1nw718wNMYcWvbJifW-6BIkoipKt4xnim/preview?usp=embed_googleplus
         //}
         // Update is called once per frame
         //void Update()
         //{
         //}
 
-        public String GetMovies(int randomNumber)
+        public String GetMovies(int randomNumber, string xmlFile)
         {
-            xmlReader = XmlReader.Create("http://meinerseitz.de/RandoMovie/MovieDatabase1000.xml");
+            xmlReader = XmlReader.Create(new StringReader(xmlFile.ToString())); //füttert den xmlReader mit dem String aus der xmlFile aus der Zeile davor
             while (xmlReader.Read())
             {
                 if ((xmlReader.NodeType == XmlNodeType.Element) && (xmlReader.Name == "Movie"))
                 {
                     if (xmlReader.HasAttributes)
                         if (xmlReader.GetAttribute("ID")==randomNumber.ToString())
-                            return ("Title: " + xmlReader.GetAttribute("title") + "\n" + "Director: " + xmlReader.GetAttribute("director") + "\n" + "Year: " + xmlReader.GetAttribute("year") + "\n" + "Country: " + xmlReader.GetAttribute("country"));
+                            return ("ID: " + xmlReader.GetAttribute("ID") + "\n" + "Title: " + xmlReader.GetAttribute("title") + "\n" + "Director: " + xmlReader.GetAttribute("director") + "\n" + "Year: " + xmlReader.GetAttribute("year") + "\n" + "Country: " + xmlReader.GetAttribute("country"));
                 }
             }
-            return ("");
+            return("nichts gefunden zum lesen");
         }
-        //{
-        //    XmlReader xmlReader = XmlReader.Create("F:/Repository/Unity/MovieRandomizer/MovieRandomizer/Assets/Database/MovieDatabase.xml");
-        //    Debug.Log(xmlReader.ReadString());
-        //}
     }
 }
